0cfe528abd6bf0648b50836564aa005d099bcf65
MadhukaRuchiranga <madhukaruchiranga2019@gmail.com>
2025-11-05 23:16:25 +0530

    feat(navbar): Updated navbar initial layout

--- client/src/assets/components/Navbar.jsx
+++ client/src/assets/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbaffr</div>
-  )
+    <div className="flex justify-between items-center">
+      <div>Talentbridge LK</div>
+      <div className="flex gap-10">
+        <div>
+          <ul className="flex gap-4">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <div>
+          <button>sign in</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
